818dd6444e4c42d738ba8c0add300fde7624733e
Sean Huber <shuber@huberry.com>
2009-10-13 15:36:46 -0700

    add generator files

---
+++ generator/public/index.php
@@ -0,0 +1,6 @@
+<?php
+
+define('APP_ROOT', dirname(dirname(__FILE__)));
+
+require_once APP_ROOT.DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'settings.php';
+require_once LIB_ROOT.DIRECTORY_SEPARATOR.'reports.php';
