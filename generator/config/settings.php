c85836d346bc8ab4aa3d7749ae8e9650b8472925
Sean Huber <shuber@huberry.com>
2009-10-13 15:43:17 -0700

    add settings.php to generator/config

---
+++ generator/config/settings.php
@@ -0,0 +1,20 @@
+<?php
+
+// Set this to wherever the reports directory is 
+//   e.g.
+//     APP_ROOT.DIRECTORY_SEPARATOR.'reports'
+//     OR
+//     '/some/shared/directory/reports'
+define('LIB_ROOT', APP_ROOT.DIRECTORY_SEPARATOR.'reports');
+
+
+// Set this to wherever your report files are stored
+//   e.g.
+//     APP_ROOT.DIRECTORY_SEPARATOR.'reports'
+define('REPORTS_ROOT', APP_ROOT.DIRECTORY_SEPARATOR.'reports');
+
+
+// Set this to wherever your template files are stored
+//   e.g.
+//     APP_ROOT.DIRECTORY_SEPARATOR.'reports'
+define('TEMPLATES_ROOT', APP_ROOT.DIRECTORY_SEPARATOR.'templates');
