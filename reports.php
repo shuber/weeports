9e6cae202cf4453f5b182e65940839755cf5c11f
Sean Huber <shuber@huberry.com>
2009-10-13 14:52:34 -0700

    add simple reports.php initializer file

---
+++ reports.php
@@ -0,0 +1,21 @@
+<?php
+
+if (!defined('DS')) define('DS', DIRECTORY_SEPARATOR);
+
+define('PUBLIC_ROOT', dirname($_SERVER['SCRIPT_FILENAME']));
+
+define('PATH_PREFIX', preg_replace('/\/'.basename(PUBLIC_ROOT).'\/'.basename($_SERVER['SCRIPT_NAME']).'/', '', $_SERVER['SCRIPT_NAME']));
+
+require_once 'lib'.DS.'functions.php';
+require_once 'lib'.DS.'environment.php';
+
+Environment::append_include_path(APP_ROOT.DS.'lib');
+Environment::append_include_path(dirname(__FILE__).DS.'lib');
+Environment::append_include_path(dirname(__FILE__).DS.'lib'.DS.'database_adapters');
+
+if (isset($_GET['debug'])) {
+    ini_set('display_errors', 1);
+    error_reporting(E_ALL);
+}
+
+echo Dispatcher::dispatch(isset($_GET['url']) ? $_GET['url'] : '');
