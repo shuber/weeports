916d4ec90c90820b2369225e613321e411c1c847
Sean Huber <shuber@huberry.com>
2009-10-15 22:13:16 -0700

    add sqlite support

--- lib/connection_manager.php
+++ lib/connection_manager.php
@@ -13,11 +13,13 @@ class ConnectionManager {
     protected static function establish_connection($name) {
         if (isset(self::$configurations[$name])) {
             $configuration = self::$configurations[$name];
+            $configuration['dbname'] = array_delete('database', $configuration);
             $adapter = array_delete('adapter', $configuration);
             $username = array_delete('username', $configuration);
             $password = array_delete('password', $configuration);
-            $configuration['dbname'] = array_delete('database', $configuration);
-            self::$connections[$name] = new PDO($adapter.':'.array_join_assoc('=', ';', $configuration), $username, $password);
+            $connection_string = $adapter.':';
+            $connection_string .= ($adapter == 'sqlite') ? $configuration['dbname'] : array_join_assoc('=', ';', $configuration);
+            self::$connections[$name] = new PDO($connection_string, $username, $password);
         } else {
             throw new InvalidArgumentException('Database configuration "'.$name.'" does not exist');
         }
